0b59076662b49e40eac261c4855af4ceecba337d
Roland Bernard <rolli.bernard@gmail.com>
2023-04-28 14:37:11 +0200

    Added menu item for normalization

---
+++ src/main/java/www/ontologyutils/protege/menu/NormalizeToSroiq.java
@@ -0,0 +1,16 @@
+package www.ontologyutils.protege.menu;
+
+import java.awt.event.ActionEvent;
+
+import org.protege.editor.owl.ui.action.ProtegeOWLAction;
+
+public class NormalizeToSroiq extends ProtegeOWLAction {
+    public void initialise() throws Exception {
+    }
+
+    public void dispose() throws Exception {
+    }
+
+    public void actionPerformed(final ActionEvent event) {
+    }
+}
