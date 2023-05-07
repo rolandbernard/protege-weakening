7be1390d6c4fe45073bde5dd9a7d7ceffbd2648f
Roland Bernard <rolli.bernard@gmail.com>
2023-05-07 17:28:26 +0200

    Added a choice dialog for which axiom to weaken to

---
+++ src/main/java/www/ontologyutils/protege/button/AbstractAxiomRefinementButton.java
@@ -0,0 +1,73 @@
+package www.ontologyutils.protege.button;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.stream.Stream;
+
+import javax.swing.JOptionPane;
+
+import org.protege.editor.core.ui.list.MListButton;
+import org.protege.editor.owl.OWLEditorKit;
+import org.semanticweb.owlapi.model.OWLAxiom;
+
+import www.ontologyutils.protege.list.AxiomListItem;
+import www.ontologyutils.refinement.*;
+import www.ontologyutils.repair.OntologyRepairRandomMcs;
+import www.ontologyutils.toolbox.*;
+
+public abstract class AbstractAxiomRefinementButton extends MListButton {
+    private AxiomListItem item;
+    private OWLEditorKit editorKit;
+
+    public AbstractAxiomRefinementButton(String name, AxiomListItem item, OWLEditorKit editorKit) {
+        super(name, Color.GREEN.darker());
+        this.item = item;
+        this.editorKit = editorKit;
+        this.setActionListener(this::performAction);
+    }
+
+    public abstract AxiomRefinement getRefinement(Ontology refOntology, Ontology fullOntology);
+
+    private OWLAxiom selectFromOptions(Stream<OWLAxiom> axioms) {
+        return (OWLAxiom) JOptionPane.showInputDialog(
+                editorKit.getOWLWorkspace(),
+                "Which axiom to weaken to?",
+                "Choose an axiom",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                axioms.toArray(),
+                null);
+    }
+
+    private void performAction(ActionEvent e) {
+        var reasonerFactory = editorKit.getOWLModelManager().getOWLReasonerManager()
+                .getCurrentReasonerFactory().getReasonerFactory();
+        var owlOntology = item.getOntology();
+        try (var ontology = Ontology.withAxiomsFrom(owlOntology, reasonerFactory)) {
+            try (var refOntology = OntologyRepairRandomMcs.forConsistency().modified(ontology)) {
+                try (var refinement = getRefinement(refOntology, ontology)) {
+                    var axiom = item.getAxiom();
+                    var newAxiom = selectFromOptions(refinement.refineAxioms(axiom));
+                    if (newAxiom == null) {
+                        return;
+                    }
+                    ontology.replaceAxiom(axiom, newAxiom);
+                }
+            }
+            ontology.applyChangesTo(owlOntology);
+        }
+    }
+
+    @Override
+    public void paintButtonContent(Graphics2D gOuter) {
+        Graphics2D g = (Graphics2D) gOuter.create();
+        int size = getBounds().height;
+        int thickness = (Math.round(size / 8.0f) / 2) * 2;
+        int x = getBounds().x;
+        int y = getBounds().y;
+        g.fillRect(x + size / 2 - thickness / 2, y + size / 4, thickness, size / 2);
+        g.rotate(Math.PI / 4, x + size / 2, y + size / 2);
+        g.fillRect(x + size / 4 + thickness / 2, y + size / 4 + thickness / 2, thickness, size / 3);
+        g.fillRect(x + size / 4 + thickness / 2, y + size / 4 + thickness / 2, size / 3, thickness);
+    }
+}
--- src/main/java/www/ontologyutils/protege/button/WeakenAxiomButton.java
+++ src/main/java/www/ontologyutils/protege/button/WeakenAxiomButton.java
@@ -1,43 +1,18 @@
 package www.ontologyutils.protege.button;
 
-import java.awt.*;
-
-import org.protege.editor.core.ui.list.MListButton;
 import org.protege.editor.owl.OWLEditorKit;
 
 import www.ontologyutils.protege.list.AxiomListItem;
-import www.ontologyutils.refinement.AxiomWeakener;
-import www.ontologyutils.repair.OntologyRepairRandomMcs;
+import www.ontologyutils.refinement.*;
 import www.ontologyutils.toolbox.*;
 
-public class WeakenAxiomButton extends MListButton {
+public class WeakenAxiomButton extends AbstractAxiomRefinementButton {
     public WeakenAxiomButton(AxiomListItem item, OWLEditorKit editorKit) {
-        super("Weaken Axiom", Color.GREEN.darker(), e -> {
-            var reasonerFactory = editorKit.getOWLModelManager().getOWLReasonerManager()
-                    .getCurrentReasonerFactory().getReasonerFactory();
-            var owlOntology = item.getOntology();
-            try (var ontology = Ontology.withAxiomsFrom(owlOntology, reasonerFactory)) {
-                try (var refOntology = OntologyRepairRandomMcs.forConsistency().modified(ontology)) {
-                    try (var weakener = new AxiomWeakener(refOntology, refOntology)) {
-                        var axiom = item.getAxiom();
-                        ontology.replaceAxiom(axiom, Utils.randomChoice(weakener.weakerAxioms(axiom)));
-                    }
-                }
-                ontology.applyChangesTo(owlOntology);
-            }
-        });
+        super("Weaken Axiom", item, editorKit);
     }
-    
+
     @Override
-    public void paintButtonContent(Graphics2D gOuter) {
-        Graphics2D g = (Graphics2D) gOuter.create();
-        int size = getBounds().height;
-        int thickness = (Math.round(size / 8.0f) / 2) * 2;
-        int x = getBounds().x;
-        int y = getBounds().y;
-        g.fillRect(x + size / 2 - thickness / 2, y + size / 4, thickness, size / 2);
-        g.rotate(Math.PI / 4, x + size / 2, y + size / 2);
-        g.fillRect(x + size / 4 + thickness / 2, y + size / 4 + thickness / 2, thickness, size / 3);
-        g.fillRect(x + size / 4 + thickness / 2, y + size / 4 + thickness / 2, size / 3, thickness);
+    public AxiomRefinement getRefinement(Ontology refOntology, Ontology fullOntology) {
+        return new AxiomWeakener(refOntology, fullOntology);
     }
 }
